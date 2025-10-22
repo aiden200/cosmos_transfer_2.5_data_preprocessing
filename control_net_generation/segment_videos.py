e7f242e082fa12fabaaf91fec6a787e494599400
aiden200 <aidenchang@gmail.com>
2025-10-22 15:30:20 -0700

    changing the sam2 callpath

--- control_net_generation/segment_videos.py
+++ control_net_generation/segment_videos.py
@@ -2,7 +2,7 @@ import subprocess, pathlib
 
 def run_sam2_pipeline(input_file, prompt_items, output_video):
     cmd = [
-            "python", "-m", "cosmos-transfer2.5.cosmos_transfer2._src.transfer2.auxiliary.sam2.sam2_pipeline",
+            "python", "cosmos-transfer2.5/cosmos_transfer2/_src/transfer2/auxiliary/sam2/sam2_pipeline.py",
             "--input_video", f"{input_file}",
             "--output_video", f"{output_video}",
             "--mode", "prompt",
