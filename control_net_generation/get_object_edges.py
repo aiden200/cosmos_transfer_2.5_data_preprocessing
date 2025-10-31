9707a83d7fabb543ab89354fd65c9e14a26cb069
aiden200 <aidenchang@gmail.com>
2025-10-30 22:00:53 -0700

    added brightness and contrast to edges

--- control_net_generation/get_object_edges.py
+++ control_net_generation/get_object_edges.py
@@ -8,6 +8,9 @@ def generate_edges(in_path, out_path):
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    bright = 50
+    contrast = 1.0
+
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # use "avc1" if you prefer H.264 and it's available
     out = cv2.VideoWriter(out_path, fourcc, fps, (w, h), isColor=False)
 
@@ -15,9 +18,11 @@ def generate_edges(in_path, out_path):
         ok, frame = cap.read()
         if not ok:
             break
+
+        frame = cv2.convertScaleAbs(frame, alpha=contrast, beta=bright)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 1.4)
-        edges = cv2.Canny(blurred, 50, 150)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 1.4)
+        edges = cv2.Canny(blurred, 10, 50)
         out.write(edges)
     cap.release()
     out.release()
